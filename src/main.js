11f058a197690889632a5d8ddf7c8573896f9ced
KhrystynaStasiv <khrystynalysanyk@gmail.com>
2024-03-26 09:39:49 +0200

    added swiper

---
+++ src/main.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".mySwiper", {
+    innerWidth: 344,
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      pagination: {
+        el: ".swiper-pagination",
+          clickable: true,
+    },
+    breakpoints: {
+        768: {
+            spaceBetween: 16,
+            loop: true,
+            slidesPerView: 2,
+            slidesPerColumn: 2
+        },
+        1280: {
+            slidesPerView: 3
+        }
+      }
+});
+    
