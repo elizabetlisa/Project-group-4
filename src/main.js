26a8e0f1ddacad9a19c6161de9711aff8553c539
Volodymyr Bilohorodskyi <niko.fm@gmail.com>
2024-03-26 11:04:44 +0200

    Revieved all new section and fixed footer

--- src/main.js
+++ src/main.js
@@ -1,2 +1,23 @@
 import modal from './js/modal';
 import slider from './js/slider';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
